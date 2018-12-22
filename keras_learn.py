20aa4fbb60bdc11719b60d4218fb1f9840bb2f97
Ashfauq Ahamed <ashfauqahamed@gmail.com>
2018-12-22 15:56:03 +0530

    Lp - update

---
+++ keras_learn.py
@@ -0,0 +1,26 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import scale
+from sklearn.model_selection import train_test_split
+X, Y = make_moons(noise=0.2, random_state=0, n_samples=1000)
+
+
+
+
+
+
+#%%
+
+a = x .copy()
+
+x.sort_values("X",ascending = False)
+
+
+
+
+
+
+#%%
